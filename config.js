8973b95d35b81beb177ad367b4c8714d0fe2a9ae
prostezmrd23 <prostezmrd23@gmail.com>
2025-04-10 11:32:25 +0200

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 module.exports = {
   TOKEN: "",
   language: "en",
-  ownerID: ["1004206704994566164", ""], 
+  ownerID: ["329486929110499329", ""], 
   mongodbUri : "",
   spotifyClientId : "",
   spotifyClientSecret : "",
